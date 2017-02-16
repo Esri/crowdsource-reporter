7509830967da9a8c03cd5b56b17e704273dc0ac4
Josh Peterson <jpeterson@esri.com>
2017-02-16 10:03:53 -0700

    add some allowed globals, relax no-console and no-unused-vars; ignore files in vendor/

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,18 @@ module.exports = {
     "parserOptions": {
         "sourceType": "module"
     },
+    "globals": {
+      "$": false,
+      "require": false,
+      "define": false,
+      "dojo": false // app should be refactored, use of global dojo is deprecated
+    },
     "rules": {
         "comma-dangle": [ "error", "never" ],
         "eqeqeq": [ "error", "always" ],
         "indent": [ "error", 2 ],
+        "no-console": [ "error", { "allow": [ "warn", "error" ] }],
+        "no-unused-vars": [ "error", { "args": "none" } ],
         "quotes": [ "error", "single" ],
         "semi": [ "error", "always" ]
     }
